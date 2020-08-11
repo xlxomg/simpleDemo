33ee13f9eb31fa2d0a3881607c60ee524d5d85e2
xieluxin <353018992@qq.com>
2020-08-11 14:55:34 +0800

    集成文件服务域sdk

--- src/main/java/com/xlx/simpledemo/SimpledemoApplication.java
+++ src/main/java/com/xlx/simpledemo/SimpledemoApplication.java
@@ -1,12 +1,16 @@
 package com.xlx.simpledemo;
 
+import com.pagoda.fastdfs.rpc.FileDomainRpcService;
+import com.pagoda.platform.contract.EnableServiceContract;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableServiceContract(clients = FileDomainRpcService.class)
 public class SimpledemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(SimpledemoApplication.class, args);
     }
 
